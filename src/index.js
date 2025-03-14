fc5b950bc373125a26eebd4b33ea8f6554a4483b
Pundima-Lakshan <pundimal@gmail.com>
2025-03-15 00:01:05 +0530

    export constants in the index

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import "@bpmn-io/form-js/dist/assets/form-js.css";
 import FormEditor from "./form-js/form-editor";
 import { default as Form } from "./form-js/form-viewer";
 
-export { Form, FormEditor };
+import * as constants from "./constants";
+
+export { Form, FormEditor, constants };
